54d0bf7fb7f9b8576df6cad6ab083e86c52ea77c
Love Efraimsson <loveefraimsson@gmail.com>
2024-03-18 13:31:55 +0100

    upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+
+function App() {
+  
+  
+
+  return (
+    <>
+      <h1>Food Tracker</h1>
+    </>
+  )
+}
+
+export default App
