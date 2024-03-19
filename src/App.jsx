515a370bebe631baf0dc76068b47364597d47b97
Love Efraimsson <loveefraimsson@gmail.com>
2024-03-19 10:49:16 +0100

    App components controlls search

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,39 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Searchbar from './components/Searchbar';
+import foodList from './food.json';
 
 
 function App() {
   
-  
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleInputChange = (event) => {
+      const { value } = event.target;
+      setSearchTerm(value);
+  };
+
+  function handleSearch() {
+      filterData(searchTerm);
+  }
+
+
+  const filterData = (searchTerm) => {
+      const filteredData = foodList.filter((item) =>
+      item.Livsmedelsnamn.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredData(filteredData);
+  };
 
   return (
     <>
       <Header />
-      <Searchbar />
+      <Searchbar 
+        searchTerm={searchTerm} 
+        handleInputChange={handleInputChange} 
+        handleSearch={handleSearch}
+      />
     </>
   )
 }
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,28 +1,9 @@
 import React, { useState, useEffect } from "react";
-import foodList from '../food.json';
-
-function Searchbar() {
-
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredData, setFilteredData] = useState(foodList);
-
-    const handleInputChange = (event) => {
-        const { value } = event.target;
-        setSearchTerm(value);
-        
-    };
-
-    function handleSearch() {
-        filterData(searchTerm);
-    }
 
 
-    const filterData = (searchTerm) => {
-        const filteredData = foodList.filter((item) =>
-        item.Livsmedelsnamn.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setFilteredData(filteredData);
-    };
+function Searchbar(props) {
+
+    
 
 
 
@@ -32,11 +13,11 @@ function Searchbar() {
             <input
                 type="text"
                 placeholder="Search..."
-                value={searchTerm}
-                onChange={handleInputChange}
+                value={props.searchTerm}
+                onChange={props.handleInputChange}
             />
 
-            <button onClick={handleSearch}>Sök!</button>      
+            <button onClick={props.handleSearch}>Sök!</button>      
         </>
     )
 }
